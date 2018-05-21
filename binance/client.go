d734c3b37956cd106c1687f4af14d805e39959ac
Ben Duncan <ben@heypiggybank.com>
2018-05-21 19:11:48 +1000

    Moved HTTP header logs to use httputil native call

--- binance/client.go
+++ binance/client.go
@@ -14,7 +14,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
+	"net/http/httputil"
 	"time"
 
 	_ "github.com/ziutek/mymysql/godrv"
@@ -53,33 +53,6 @@ func NewClient(key, secret string) (c *Client) {
 		httpClient: &http.Client{},
 	}
 	return client
-}
-
-// formatRequest generates ascii representation of a request
-func formatRequest(r *http.Request) string {
-	// Create return string
-	var request []string
-	// Add the request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)
-	// Add the host
-	request = append(request, fmt.Sprintf("Host: %v", r.Host))
-	// Loop through headers
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	// If this is a POST, add post data
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-	}
-	// Return the request as a string
-	return strings.Join(request, "\n")
 }
 
 func (c *Client) do(method, resource, payload string, auth bool, result interface{}) (resp *http.Response, err error) {
@@ -124,7 +97,8 @@ func (c *Client) do(method, resource, payload string, auth bool, result interfac
 		rawLog.QueryURL = req.URL.String()
 		rawLog.QueryDate = time.Now()
 
-		rawLog.QueryHeaders = []byte(formatRequest(req))
+		rawLog.QueryHeaders, _ = httputil.DumpRequest(req, false)
+		//[]byte(formatRequest(req))
 
 		err = dbmap.Insert(&rawLog)
 
