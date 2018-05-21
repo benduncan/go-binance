c797518c993a842d3222874faa3364cc3f6e736a
Ben Duncan <ben@heypiggybank.com>
2018-05-21 15:39:10 +1000

    Adding verbose client<>server logging for auditing binance API transactions

--- binance/binance.go
+++ binance/binance.go
@@ -11,7 +11,18 @@
 */
 package binance
 
+import (
+	"database/sql"
+	"time"
+
+	gorp "gopkg.in/gorp.v2"
+)
+
 //"errors"
+
+// DBI string for logging verbose HTTP headers/response
+var dbmap gorp.DbMap
+var verboseLogging bool
 
 const (
 	BaseUrl = "https://api.binance.com"
@@ -19,6 +30,17 @@ const (
 
 type Binance struct {
 	client *Client
+}
+
+type HTTPLog struct {
+	TransactionID int64
+	QueryURL      string
+	QueryHeaders  []byte
+	QueryDate     time.Time
+	ResponseBody  []byte
+	ResponseCode  int
+	ResponseDate  time.Time
+	ID            int64
 }
 
 /*
@@ -31,7 +53,26 @@ func handleErr(r jsonResponse) error {
     return nil
 }
 */
-func New(key, secret string) *Binance {
+func New(key, secret, dbi string) *Binance {
 	client := NewClient(key, secret)
+
+	// DB logging enabled? Connect to the specified DBI source
+	if dbi != "" {
+
+		db, err := sql.Open("mymysql", dbi)
+
+		// DB error? Panic
+		if err != nil {
+			panic(err)
+		}
+
+		// Connect to the DB and provision the DB table if missing
+		dbmap = gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+		dbmap.AddTableWithName(HTTPLog{}, "HTTPLog").SetKeys(true, "ID")
+
+		// Log all requests
+		verboseLogging = true
+	}
+
 	return &Binance{client}
 }
--- binance/client.go
+++ binance/client.go
@@ -5,6 +5,7 @@
 package binance
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
@@ -13,7 +14,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
+
+	_ "github.com/ziutek/mymysql/godrv"
+
+	_ "github.com/ziutek/mymysql/native" // Native engine
 )
 
 type Client struct {
@@ -49,7 +55,36 @@ func NewClient(key, secret string) (c *Client) {
 	return client
 }
 
+// formatRequest generates ascii representation of a request
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	// Return the request as a string
+	return strings.Join(request, "\n")
+}
+
 func (c *Client) do(method, resource, payload string, auth bool, result interface{}) (resp *http.Response, err error) {
+
+	rawLog := HTTPLog{}
 
 	fullUrl := fmt.Sprintf("%s/%s", BaseUrl, resource)
 
@@ -84,14 +119,56 @@ func (c *Client) do(method, resource, payload string, auth bool, result interfac
 		req.URL.RawQuery = q.Encode() + "&signature=" + signature
 	}
 
+	// Log the request via the specified DBI, use as an audit trail
+	if verboseLogging == true {
+		rawLog.QueryURL = req.URL.String()
+		rawLog.QueryDate = time.Now()
+
+		rawLog.QueryHeaders = []byte(formatRequest(req))
+
+		err = dbmap.Insert(&rawLog)
+
+		if err != nil {
+			return
+		}
+
+	}
+
+	//fmt.Println("Request =>", req)
 	resp, err = c.httpClient.Do(req)
+
 	if err != nil {
 		return
+	}
+
+	// Response success? Log it
+	if verboseLogging == true {
+		rawLog.ResponseCode = resp.StatusCode
+
+		// Duplicate the body content, since ioutil.ReadAll is one-time only, and we need to replace it for the json decoder to function
+		var bodyBytes []byte
+
+		if resp.Body != nil {
+			bodyBytes, _ = ioutil.ReadAll(resp.Body)
+		}
+
+		// Restore the io.ReadCloser to its original state
+		resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+		// Log the raw body
+		rawLog.ResponseBody = bodyBytes
+
+		// Log the response time
+		rawLog.ResponseDate = time.Now()
+
+		// Update the log to reflect the body
+		dbmap.Update(&rawLog)
 	}
 
 	// Check for error
 	defer resp.Body.Close()
 	err = handleError(resp)
+
 	if err != nil {
 		return
 	}
@@ -101,5 +178,6 @@ func (c *Client) do(method, resource, payload string, auth bool, result interfac
 		decoder := json.NewDecoder(resp.Body)
 		err = decoder.Decode(result)
 	}
+
 	return
 }
